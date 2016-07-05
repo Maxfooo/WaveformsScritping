059de1f7d37859b3b65c85f64fb6b52807d92349
Max <maxfooo@gmail.com>
2016-07-05 11:55:13 -0700

    Update SampleSine_V2.js

--- SampleSine_V2.js
+++ SampleSine_V2.js
@@ -3,9 +3,10 @@
 
 
 function main() {
-   var maxVal = power(2, 12);
+   var maxVal = Math.pow(2,12);
    var stepVal = 0;
    ch = Scope1.Channel1;
+   mtr = Meter.Meters.C1DC
    var filePath = "C:/Users/maxr/Desktop/WaveformWorkspace/";
    var fileName = "measureDAC"
    var averageTestFile = File(filePath+fileName+"_Averages"+".csv");
@@ -13,41 +14,22 @@ function main() {
 
 	if(!('Patterns1' in this) && !('Scope1' in this)) 
 	{
-		throw("Please open Patterns and Scope instrument");
+   	throw("Please open Patterns and Scope instrument");
 	}
 
    Patterns1.Channels.Bus1.Number.value = stepVal;
    Patterns1.run();
-   //Scope1.run();
+   Scope1.run();
    for(stepVal; stepVal < maxVal; stepVal++)
    {
-
-   Patterns1.Channels.Bus1.Number.value = stepVal;
-   averageTestFile.appendLine(ch.measure("Average"));
-/*
-   Scope1.single();
-   Scope1.wait();
-   Patterns1.Channels.Bus1.Number.value = stepVal;
-   var data = ch.data;
-   data.forEach(function(sample){average += sample})
-   average /= data.length;
-   averageTestFile.appendLine(average);
-*/
-   
-	  
+      Patterns1.Channels.Bus1.Number.value = stepVal;
+      print(mtr.value);
+      //print(ch.measure("Average"));
+      //averageTestFile.appendLine(ch.measure("Average"));
+      averageTestFile.appendLine(mtr.value);      
    }
    
    Patterns1.stop();
    Scope1.stop();
 }
-
-function power(base, raise) {
-   var result = base;
-   for(var i = 0; i < (raise - 1); i++)
-   {
-      result *= base;
-   }
-   return result;
-}
-
 main();
